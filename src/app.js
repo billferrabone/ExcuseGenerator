a5c2e6f266b258b7597cc45af5dbdbb89177e9f7
billferrabone <billferrabone@gmail.com>
2022-08-06 02:47:41 +0000

    Generacion

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who1 = Math.floor(Math.random() * 3);
+  let action1 = Math.floor(Math.random() * 3);
+  let what1 = Math.floor(Math.random() * 3);
+  let when1 = Math.floor(Math.random() * 3);
+
+  document.getElementById("excuse").innerHTML =
+    who[who1] + action[action1] + what[what1] + when[when1];
 };
